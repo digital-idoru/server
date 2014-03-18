0bdcdead9581f5fadc45d6df2f5066f15033e297
Daniel S Hono II <dhono@albany.edu>
2014-03-18 14:52:57 -0400

    Fixing some error handling in the client.

--- client.c
+++ client.c
@@ -28,29 +28,29 @@ void setAddressInfo(struct addrinfo**);
 int main(void) {
 
 	struct addrinfo *res;
-
+	int sock;
 
 	/*Set up the address info */
 	setAddressInfo(&res);
 
 
-	freeaddrinfo(res);
+	freeaddrinfo(res); //free the results linked list when we're done 
 	return 0;
 }
 
 void setAddressInfo(struct addrinfo **resultList) {
 
-	struct addrinfo hints;
+	struct addrinfo hints; //hints structure 
 	int status; 
 
 	memset(&hints, 0, sizeof hints);
 
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE; 
+	hints.ai_family = AF_UNSPEC; //v4 or v5
+	hints.ai_socktype = SOCK_STREAM; //TCP
+	hints.ai_flags = AI_PASSIVE; //fill in my ip for me. 
 
 	if( (status = getaddrinfo(NULL, PORT, &hints, resultList)) != 0) 
-		fprintf(stderr, "Could not getaddrinfo~!\n"); exit(EXIT_FAILURE);
+		fprintf(stderr, "Could not getaddrinfo~! %s\n", gai_strerror(status)); exit(EXIT_FAILURE);
 
 
 	return;
