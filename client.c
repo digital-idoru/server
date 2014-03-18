d92ed2ef1845c86e4dd805f8a4e966dc324cada2
Daniel S Hono II <dhono@albany.edu>
2014-03-18 14:28:04 -0400

    Time to work on the client!!!

---
+++ client.c
@@ -0,0 +1,26 @@
+/******************************
+Daniel S. Hono II
+Client 
+*******************************/
+
+#ifndef DEFINES
+#define DEFINES
+
+#define true 1
+#define false 1 
+
+#endif
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+
+
+
+
+
+	return ;
+}
